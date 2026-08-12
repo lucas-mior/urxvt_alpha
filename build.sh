c70da6c6b6692ef6e8727b37f693a36fbbb9db44
lucas-mior <lucas.mior@tutamail.com>
2026-08-11 22:26:31 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ dir=$(dirname "$(readlink -f "$0")")
 cd "$dir" || exit
 program=$(get_program "$0")
 script=$(basename "$0")
-target="${1:-debug}"
+build_parse_args "$@"
 
-printf "\n${script} ${RED}${1:-} ${2:-}$RES\n"
+build_print_invocation "$script"
 
 PREFIX="${PREFIX:-/usr/local}"
 DESTDIR="${DESTDIR:-/}"
@@ -19,7 +19,7 @@ DESTDIR="${DESTDIR:-/}"
 exe="bin/$program"
 mkdir -p "$(dirname "$exe")"
 
-CC=$(get_compiler "$target")
+CC=$(get_compiler "$mode")
 
 CPPFLAGS="$CPPFLAGS -I$dir/cbase"
 
@@ -51,7 +51,7 @@ fi
 
 LDFLAGS="$LDFLAGS -lm $(pkg-config libmagic --libs)"
 
-case "$target" in
+case "$mode" in
 debug)
     CFLAGS="$CFLAGS -g3 -Og -fsanitize=undefined"
     CPPFLAGS="$CPPFLAGS -DDEBUGGING=1"
@@ -76,7 +76,7 @@ build_program () {
     trace_off
 }
 
-case "$target" in
+case "$mode" in
 fast_feedback)
     build_program
     LC_ALL=C "$exe"
@@ -97,7 +97,7 @@ install)
     trace_off
     ;;
 test)
-    TEST_EXCLUDE_PATTERN='(^|/)cbase/' test "$2"
+    TEST_EXCLUDE_PATTERN='(^|/)cbase/' test "$target"
     exit
     ;;
 check)
